6de817277529be5d67919ab9cfc70ee5621a7a1a
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-22 18:13:29 -0700

    Importing dependencies

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const questions = [
-
-];
-
-function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
-
-init();
+var util = require("util");
+var fs = require("fs");
+var axios = require("axios");
+var inquirer = require("inquirer");
