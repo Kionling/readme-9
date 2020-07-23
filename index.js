7e60a054e9fb73fd7db4bdd0155f5fa4d6e88e51
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-22 18:21:39 -0700

    Generating inquirer prompts for every question

--- index.js
+++ index.js
@@ -2,3 +2,60 @@ var util = require("util");
 var fs = require("fs");
 var axios = require("axios");
 var inquirer = require("inquirer");
+
+
+var questions = [
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "list",
+        message: "",
+        choices: [
+            'MIT',
+            'GPL',
+            'ISC'
+
+        ],
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+
+    
+]
